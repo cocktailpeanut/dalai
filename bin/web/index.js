97668629974e0fb7a20ba0345523131bbbd0e883
keldenl <keldenlin@gmail.com>
2023-03-21 13:10:38 -0700

    update the BE to accomodate for custom and default prompts, so future updates to default prompts won't overwrite the custom ones

--- bin/web/index.js
+++ bin/web/index.js
@@ -17,20 +17,45 @@ const start = (port, home) => {
         res.render("index")
     })
     app.get("/prompts", (req, res) => {
-        const promptsDir = path.join(__dirname, "prompts");
-        fs.readdir(promptsDir, (err, files) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ error: "Internal server error" });
+        const promptsDirs = [
+            path.join(__dirname, "prompts"),
+            path.join(__dirname, "prompts", "custom")
+        ];
+        const allPrompts = [];
+        let numDirsChecked = 0;
+        promptsDirs.forEach(promptsDir => {
+            if (fs.existsSync(promptsDir) && fs.statSync(promptsDir).isDirectory()) {
+                fs.readdir(promptsDir, (err, files) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).json({ error: "Internal server error" });
+                    }
+                    const prompts = files.filter(file => !fs.statSync(path.join(promptsDir, file)).isDirectory())
+                        .map((file) => {
+                            const promptName = path.basename(file, ".txt");
+                            const promptValue = fs.readFileSync(path.join(promptsDir, file), "utf8");
+                            return { name: promptName, value: promptValue };
+                        });
+                    allPrompts.push(...prompts);
+                    console.log(`Added ${prompts.length} prompts from ${promptsDir}`);
+                    numDirsChecked++;
+                    if (numDirsChecked === promptsDirs.length) {
+                        res.json(allPrompts);
+                        console.log(`Returning ${allPrompts.length} prompts`);
+                    }
+                });
+            } else {
+                console.error(`Directory not found: ${promptsDir}`);
+                numDirsChecked++;
+                if (numDirsChecked === promptsDirs.length) {
+                    res.json(allPrompts);
+                    console.log(`Returning ${allPrompts.length} prompts`);
+                }
             }
-            const prompts = files.map((file) => {
-                const promptName = path.basename(file, ".txt");
-                const promptValue = fs.readFileSync(path.join(promptsDir, file), "utf8");
-                return { name: promptName, value: promptValue };
-            });
-            res.json(prompts);
         });
     });
+
+
 
     httpServer.listen(port, () => {
         console.log(`Server running on http://localhost:${port}/`)
