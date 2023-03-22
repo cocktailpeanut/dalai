dde228fa37cd598029fe55ed43c44fdcef938640
cocktailpeanut <cocktailpeanuts@proton.me>
2023-03-22 13:57:00 -0400

    fixes
    1. now that prompts are more complex, do NOT submit with enter. Submit is now only activated by pressing the "Go" button
    2. Cache the selected model in browser
    3. Windows Encoding is solved (escape sequences)

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ const winEscape = (str) => {
   .replaceAll(/\\r/g, "\r")
   .replaceAll(/\\t/g, "\t")
   .replaceAll(/\\b/g, "\b")
-  .replaceAll(/\\f/g, "/f")
+  .replaceAll(/\\f/g, "\f")
+  .replaceAll(/\\/g, "")
 }
 
 class Dalai {
