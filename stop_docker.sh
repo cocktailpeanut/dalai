da3a740add4762e255eaeac4ed7598a8c69b289d
Camilo Giraldo <camigira@gmail.com>
2023-04-02 03:27:15 +1000

    Update stop_docker.sh

--- stop_docker.sh
+++ stop_docker.sh
@@ -2,7 +2,7 @@
 
 #Check if Docker is running
 if ! docker info > /dev/null 2>&1; then
-    echo "This script uses Docker and is currently not runing, please start Docker and try again!"
+    echo "This script uses Docker and is currently not running, please start Docker and try again!"
     exit 1
 else
     # Start dalai alpaca 7B model
